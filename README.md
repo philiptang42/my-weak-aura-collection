95dfa0b474b91b2c7d86e0e4630cec111d81543b
philiptang42 <philiptang42@gmail.com>
2015-09-25 18:22:14 -0400

    initialize repository

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Weak Aura Collection
+
+These WeakAuras are intended for the following conditions:
+* Default UI setup at 100% zoom
+* Bottom-left and bottom-right action bars enabled
+
+# Notes
+
+### Warrior
+* Tracks Rend duration and glows at optimal refresh time.
+* Enrage is in here, but Arms doesn't even have it anymore. Derp.
---
+++ Warrior/arms-warrior.lua
@@ -0,0 +1,109 @@
+{
+	["backdropColor"] = {
+    	[1] = 1,
+    	[2] = 1,
+    	[3] = 1,
+    	[4] = 0.5,
+	},
+	["controlledChildren"] = {
+    	[1] = "Legendary Ring 3",
+    	[2] = "Legendary Ring Final Form",
+    	[3] = "Enrage",
+    	[4] = "Colossus Smash",
+    	[5] = "Rend Refresh",
+    	[6] = "Rend",
+    	[7] = "Sweeping Strikes",
+    	[8] = "Sudden Death",
+    	[9] = "Bladestorm",
+    	[10] = "Adrenaline Rush  2",
+	},
+	["borderBackdrop"] = "Blizzard Tooltip",
+	["xOffset"] = 0,
+	["border"] = false,
+	["yOffset"] = 0,
+	["anchorPoint"] = "CENTER",
+	["borderSize"] = 16,
+	["borderColor"] = {
+    	[1] = 1,
+    	[2] = 1,
+    	[3] = 1,
+    	[4] = 0.5,
+	},
+	["expanded"] = true,
+	["borderOffset"] = 5,
+	["selfPoint"] = "BOTTOMLEFT",
+	["additional_triggers"] = {
+	},
+	["id"] = "Warrior",
+	["frameStrata"] = 1,
+	["untrigger"] = {
+	},
+	["actions"] = {
+    	["start"] = {
+    	},
+    	["init"] = {
+    	},
+    	["finish"] = {
+    	},
+	},
+	["borderInset"] = 11,
+	["numTriggers"] = 1,
+	["borderEdge"] = "None",
+	["trigger"] = {
+    	["subeventPrefix"] = "SPELL",
+    	["type"] = "aura",
+    	["spellIds"] = {
+    	},
+    	["subeventSuffix"] = "_CAST_START",
+    	["debuffType"] = "HELPFUL",
+    	["names"] = {
+    	},
+    	["event"] = "Health",
+    	["unit"] = "player",
+	},
+	["animation"] = {
+    	["start"] = {
+        	["duration_type"] = "seconds",
+        	["type"] = "none",
+    	},
+    	["main"] = {
+        	["duration_type"] = "seconds",
+        	["type"] = "none",
+    	},
+    	["finish"] = {
+        	["duration_type"] = "seconds",
+        	["type"] = "none",
+    	},
+	},
+	["load"] = {
+    	["difficulty"] = {
+        	["multi"] = {
+        	},
+    	},
+    	["role"] = {
+        	["multi"] = {
+        	},
+    	},
+    	["use_class"] = "true",
+    	["talent"] = {
+        	["multi"] = {
+        	},
+    	},
+    	["class"] = {
+        	["single"] = "WARRIOR",
+    	},
+    	["spec"] = {
+        	["multi"] = {
+        	},
+    	},
+    	["race"] = {
+        	["multi"] = {
+        	},
+    	},
+    	["size"] = {
+        	["multi"] = {
+        	},
+    	},
+	},
+	["regionType"] = "group",
+}
